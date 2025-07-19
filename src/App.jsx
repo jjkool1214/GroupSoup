09ef45526096792022270602f24dccd6730e814a
jjkool1214 <jaysenstolarz@gmail.com>
2025-07-19 14:20:14 -0400

    Finishing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
 import './App.css';
 
-const Home = lazy(() => import('./components/Home.jsx'));
-const Account = lazy(() => import('./components/Account.jsx'));
-const Groups = lazy(() => import('./components/Groups.jsx'));
-const Landing = lazy(() => import('./components/Landing.jsx'));
-const Login = lazy(() => import('./components/Login.jsx'));
-const SignUp = lazy(() => import('./components/SignUp.jsx'));
-
+const Home = lazy(() => import('./components/Home'));
+const Account = lazy(() => import('./components/Account'));
+const Groups = lazy(() => import('./components/Groups'));
+const Landing = lazy(() => import('./components/Landing'));
+const Login = lazy(() => import('./components/Login'));
+const SignUp = lazy(() => import('./components/SignUp'));
+const SetUpOne = lazy(() => import('./components/SetUpOne'));
+const SetUpTwo = lazy(() => import('./components/SetUpTwo'));
 
 
 function App() {
     return (
         <Router>
+
+            <ScrollToTop />
+
             <div className="App">
                 <main>
                     <Suspense fallback={<div>Loading...</div>}>
@@ -24,6 +29,8 @@ function App() {
                             <Route path="/groups" element={<Groups />} />
                             <Route path="/home" element={<Home />} />
                             <Route path="/signup" element={<SignUp />} />
+                            <Route path="/signup/stepone" element={<SetUpOne />} />
+                            <Route path="/signup/steptwo" element={<SetUpTwo />} />
                         </Routes>
                     </Suspense>
                 </main>
